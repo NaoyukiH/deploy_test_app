490c95b9cc0aaa2086c274f82db8c96aeac36735
Naoyuki Hamaguchi <naorkz@gmail.com>
2017-04-25 14:58:46 +0000

    s3設定 xxx

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 SampleApp::Application.initialize!
-
-config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-      s3_endpoint: "s3-ap-northeast-1.amazonaws.com",
-    }
-  }
